68c4f7e93c4a003767d1602b73c5b6a64a762662
andres0428 <joannyandres@gmail.com>
2020-05-28 18:37:04 -0500

    change connection

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,18 @@ namespace Login
 
         private void EntrarButton_Click(object sender, EventArgs e)
         {
-          
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Administracion;Integrated Security=True");
+                con.Open();
+                MessageBox.Show("conexion true");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("conexion false", ex.Message);
+            }
         }
 
         private void IDLabel_Click(object sender, EventArgs e)
